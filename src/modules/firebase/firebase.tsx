0d21f4236ca9d3aef964835e2547210b72cc853f
Marko Krajina <mkrajina@koteks.local>
2021-07-22 10:56:36 +0200

    Firebase config fix

--- src/modules/firebase/firebase.tsx
+++ src/modules/firebase/firebase.tsx
@@ -2,12 +2,13 @@ import firebase from "firebase"
 
 const config = {
   apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
+  authDomain: "pizza-a-tron-df5e4.firebaseapp.com",
+  databaseURL:
+    "https://pizza-a-tron-df5e4-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "pizza-a-tron-df5e4",
+  storageBucket: "pizza-a-tron-df5e4.appspot.com",
+  messagingSenderId: "435361502043",
+  appId: "1:435361502043:web:40fdad24e38794238b807e",
 }
 
 if (!firebase.apps.length) {
